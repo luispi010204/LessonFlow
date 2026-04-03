6e11db8bdee1386f9cb1f0889a1851220c442e75
luispi010204 <spinalui@students.zhaw.ch>
2026-04-03 14:10:43 +0200

    initial Commit

---
+++ src/main/java/zhaw/ch/lessonflow/LessonflowApplication.java
@@ -0,0 +1,13 @@
+package zhaw.ch.lessonflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zhaw/ch/lessonflow/LessonflowApplicationTests.java
@@ -0,0 +1,13 @@
+package zhaw.ch.lessonflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
